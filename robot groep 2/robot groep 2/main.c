42dd39d35b47852a1713314cdde8a70a5ff1c2a6
Erik Bussing <e.a.b.bussing@student.tudelft.nl>
2017-03-10 14:43:22 +0100

    the aansluitingen file voor de rp6 robot, gebruik deze in het programma made a lot of to do functions in main

--- robot groep 2/robot groep 2/main.c
+++ robot groep 2/robot groep 2/main.c
@@ -6,13 +6,53 @@
  */ 
 
 #include <avr/io.h>
+#include "rp6aansluitingen.h"
 
+void initMotors();
+void setMotors(int left, int right);
+int getLeftMotor();
+int getRightMotor();
+
+void initCommunication();
 
 int main(void)
 {
+	initMotors();
+	initUsart();
+	
 	
     while (1) 
     {
     }
 }
 
+/************************************************************************/
+/* initialize the motors pwm system                                     */
+/************************************************************************/
+void initMotors(){
+	// TODO
+}
+
+/************************************************************************/
+/* set the motors, if null as input motor won't be changed              */
+/************************************************************************/
+void setMotors(int left, int right){
+	// TODO
+}
+
+/************************************************************************/
+/* returns current speed of the motor                                   */
+/************************************************************************/
+int getLeftMotor(){
+	// TODO
+}
+int getLeftMotor(){
+	// TODO
+}
+
+/************************************************************************/
+/* initialises i2c communication with Arduino                           */
+/************************************************************************/
+void initCommunication(){
+	// TODO
+}
