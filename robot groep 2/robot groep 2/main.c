3f63782bc3bd130f9b17e460ef1917a15b664716
Erik Bussing <e.a.b.bussing@student.tudelft.nl>
2017-03-12 19:29:07 +0100

    updated initmotors to use 8 bit phase corrected pwm

--- robot groep 2/robot groep 2/main.c
+++ robot groep 2/robot groep 2/main.c
@@ -25,7 +25,7 @@ void emergencyBrake();
 // communication functions
 void initCommunication();
 
-int rightDSpeed;		// these variables are used to store the desired speed between -maxspeed and +maxspeed
+int rightDSpeed;		// these variables are used to store the desired speed between -maxspeed(-255) and +maxspeed(255)
 int leftDSpeed;			// they are used to adjust motor speed accordingly in the main while loop
 
 
@@ -51,32 +51,30 @@ int main(void)
 /* initialize the motors pwm system                                     */
 /************************************************************************/
 void initMotors(){
-	// TODO set to 8 bit mode, phase corrected
-	TCCR1A = 1 << WGM11 | (1 << WGM10);		// fast pwm w/ OCR1x
-	TCCR1B = 1 << WGM13 | (1 << WGM12);
+	// set the timer registers
+	TCCR1A = 1 << WGM10;					// phase corrected pwm 8 bit w/ OCR1x
 
 	TCCR1A |= 1 << COM1A1 | (1 << COM1B1);	// non inverted mode on both motors
 
 	TCCR1B |= 1 << CS10;					// no prescaler
 
+	TIMSK = 1 << OCIE1A | (1 << OCIE1B);	// enable the timer interrupt mask bits
+
+	// set the motor registers 
 	DDRC |= DIR_R | DIR_L;					// set direction pins as output
 	DDRD |= MOTOR_R | MOTOR_L;				// MOTOR_R & MOTOR_L as output
 
 	PORTC |= DIR_R | DIR_L;					// set motor direction to ???
-	OCR1A = OCR1B = 0;						// initialize motors with no speed
+	OCR1A = OCR1B = 0;						// initialize motor pwm timers with no speed
 }
 
 /************************************************************************/
 /* set the motors                                                       */
 /************************************************************************/
 void setMotors(int left, int right){
-	// TODO streamline function
-	if(right > MAXSPEED) right = MAXSPEED;
-	else if(right < -MAXSPEED) right = -MAXSPEED;
-	MOTORSPEED_R = 0xff & right;
-	if(left > MAXSPEED) right = MAXSPEED;
-	else if(left < -MAXSPEED) right = -MAXSPEED;
-	MOTORSPEED_L = 0xff & left;
+	// TODO streamline code
+	MOTORSPEED_R = right;
+	MOTORSPEED_L = left;
 
 	// set direction so ports can be adjusted as necessary
 	int direction = 0;
