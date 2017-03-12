fa11de78075351f4425bc438f4cd1733c2de0814
Erik Bussing <e.a.b.bussing@student.tudelft.nl>
2017-03-12 13:26:58 +0100

    made the arduino project in the arduino folder, please add all code for the arduino here

---
+++ arduino/arduino/main.c
@@ -0,0 +1,18 @@
+/*
+ * arduino.c
+ *
+ * Created: 12-3-2017 13:25:33
+ * Author : Erik
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
