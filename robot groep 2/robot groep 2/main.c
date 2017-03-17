0c332cefb63e2a4c381814b1ffa6c2f02b31ba02
Erik Bussing <e.a.b.bussing@student.tudelft.nl>
2017-03-17 09:33:59 +0100

    fixed the motors, now going to put them in a different file

--- robot groep 2/robot groep 2/main.c
+++ robot groep 2/robot groep 2/main.c
@@ -6,7 +6,6 @@
  */ 
  
  #include "rp6aansluitingen.h"
- #include "i2c.h"
  #include <stdint.h>
  #include <stdlib.h>
  #include <util/delay.h>
@@ -18,8 +17,10 @@
  #define MOTORSPEED_R				OCR1A
  #define MOTORSPEED_L				OCR1B
  
- #define MOTOR_ADJUST_FREQUENTIE	100
- #define MOTOR_ADJUST_DELAY			F_CPU / MOTOR_ADJUST_FREQUENTIE / 1024
+
+ #define MOTOR_ADJUST_FREQUENTIE	50
+ #define MOTOR_ADJUST_TIME			0.5
+ #define MOTOR_ADJUST_DELAY			F_CPU / MOTOR_ADJUST_FREQUENTIE * MOTOR_ADJUST_TIME / 1024
 
  #define TRUE 0xFF;
  #define FALSE 0;
@@ -29,6 +30,8 @@
  // movement functions
  void initMotors();
  void setMotors(int left, int right);
+ void setLeftMotor(int speed);
+ void setRightMotor(int speed);
  void emergencyBrake();
  
  // global variables
@@ -54,6 +57,8 @@
 	 initMotors();
 
 	 sei();
+	 
+	 rightDesiredSpeed = -200;
 
 	 while (1)
 	 {
@@ -66,17 +71,16 @@
  /************************************************************************/
  void initMotors(){
 	 // set the PWM timer registers
-	 TCCR1A = 1 << WGM10;					// phase corrected PWM 8 bit w/ ICR1 and OCR1x
-	 ICR1 = 210;							// max safe power
+	 TCCR1A = 1 << WGM10;					// phase corrected PWM 8 bit w/ OCR1x
 	 TCCR1A |= 1 << COM1A1 | (1 << COM1B1);	// non inverted mode on both motors
 	 TCCR1B |= 1 << CS10;					// no prescaler
 
 
 	 // sets timer0 for acceleration and deceleration
-	 TCCR0 = WGM01;							// CTC mode
-	 TCCR0 |= 1 << CS00 | (1 << CS02);		// 1024 prescaler
-	 TIMSK |= 1 << OCIE0;					// enable the timers interrupt mask bit
-	 OCR0 = MOTOR_ADJUST_DELAY;				// set the compare register for timer0
+	 TCCR2 = WGM21;							// CTC mode
+	 TCCR2 |= 1 << CS20 | (1 << CS22);		// 1024 prescaler
+	 TIMSK |= 1 << OCIE2;					// enable the timers interrupt mask bit
+	 OCR2 = MOTOR_ADJUST_DELAY;				// set the compare register for timer0
 
 	 
 	 // set the motor registers
@@ -90,42 +94,29 @@
  /* set the motors                                                       */
  /************************************************************************/
  void setMotors(int left, int right){
-	// adjusts the motors PWM OCR's if necessary
-	 if(MOTORSPEED_R != right) MOTORSPEED_R = abs(right);
-	 if(MOTORSPEED_L != left) MOTORSPEED_L = abs(left);
-
-
-	 // set direction so ports can be adjusted as necessary
-	 int direction = 0;
-	 if(right < 0) direction |= DIR_R;
-	 if(left < 0) direction |= DIR_L;
-	 PORTC = (PORTC & ~(1 << DIR_R | ( 1 << DIR_L))) | direction;
+	setLeftMotor(left);
+	setRightMotor(right);
  }
+
+ void setLeftMotor(int speed){
+	int absSpeed = abs(speed);
+	if(MOTORSPEED_L != absSpeed) MOTORSPEED_L = absSpeed;
+
+	speed < 0 ? (PORTC |= DIR_L) : (PORTC &= ~DIR_L);
+ }
+
+  void setRightMotor(int speed){
+	  int absSpeed = abs(speed);
+	  if(MOTORSPEED_R != absSpeed) MOTORSPEED_R = absSpeed;
+
+	  speed < 0 ? (PORTC |= DIR_R) : (PORTC &= ~DIR_R);
+  }
 
  /************************************************************************/
  /* initializes the communications with arduino via i2c                  */
  /************************************************************************/
  void initCommunication(){
-	initUSART();
-	init_i2c_slave(8);
-	init_i2c_verzend(verzendByte);
-	init_i2c_ontvang(ontvangData);
-
- }
-
-  /*slave heeft data ontvangen van de master
- data[] een array waarin de ontvangen data staat
- tel het aantal bytes dat ontvangen is*/
- 
-void ontvangData(uint8_t data[],uint8_t tel){
-	for(int i=0;i<tel;++i)
-	    data_ont[i]=data[i];
-	data_flag = TRUE;
-	writeString("o\n\r");
-}
-
- uint8_t verzendByte(){
-	return databyte++;
+	// TODO
  }
 
  /************************************************************************/
@@ -136,24 +127,26 @@ void ontvangData(uint8_t data[],uint8_t tel){
 	 rightDesiredSpeed = leftDesiredSpeed = 0;
  }
 
-/*  ISR(TWI_vect) {
-
-	 slaaftwi();
-
- }*/
-
  /************************************************************************/
  /* chances the speed of the motors on timer0 COMP interrupt             */
  /************************************************************************/
- ISR(TIMER0_COMP_vect){
- /*
+ ISR(TIMER2_COMP_vect){
 	int rightTarget = MOTORSPEED_R;
+	if(PORTC & DIR_R) rightTarget = -rightTarget;
 	int leftTarget = MOTORSPEED_L;
+	if(PORTC & DIR_L) leftTarget = -leftTarget;
 
-	if(rightDesiredSpeed != rightTarget)
-		rightTarget += (rightDesiredSpeed - rightTarget) / MOTOR_ADJUST_FREQUENTIE + 1;
-	if(leftDesiredSpeed != leftTarget)
-		leftTarget += (leftDesiredSpeed - leftTarget) / MOTOR_ADJUST_FREQUENTIE + 1;
- 
-	 setMotors(leftTarget, rightTarget);*/
+	if(rightDesiredSpeed != rightTarget){
+		rightTarget += (rightDesiredSpeed - rightTarget) / MOTOR_ADJUST_FREQUENTIE;
+		rightDesiredSpeed > rightTarget ? setRightMotor(++rightTarget) : setRightMotor(--rightTarget);
+	}
+	if(leftDesiredSpeed != leftTarget){
+		leftTarget += (leftDesiredSpeed - leftTarget) / MOTOR_ADJUST_FREQUENTIE;
+		leftDesiredSpeed > leftTarget ? setLeftMotor(++leftTarget) : setLeftMotor(--leftTarget);
+	}
+
+	writeInteger(rightTarget, 10);
+	writeString("  ");
+	writeInteger(leftTarget, 10);
+	writeString("\n\r");
  }
