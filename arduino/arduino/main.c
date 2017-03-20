65c1a2b20710787594be0649c621bbec5765528e
GioNees <giovannineeskens99@gmail.com>
2017-03-20 14:02:57 +0100

    sorry guys

--- arduino/arduino/main.c
+++ arduino/arduino/main.c
@@ -7,9 +7,17 @@
 #define F_CPU 16000000
 #define DEVICE_ADRES   8
 
+#define US_PORT			PORTD //the port of the sensor
+#define US_PIN			PIND //the pin of the sensor
+#define US_DDR			DDRD
+#define US_POS			PD5 //PORTD5
+#define US_ERROR		0xffff //time-out system
+#define US_NO_OBSTACLE	0xfffe // no obstacle in range
+
 #include "../../shared/twi_codes.h"
 #include "i2c_mst.h"
 #include <avr/io.h>
+#include <stdlib.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdint.h>
@@ -17,6 +25,9 @@
 // functions
 void (*mode) (uint8_t);
 void setMode(uint8_t set);
+void verzenden_array(uint8_t address, uint8_t b[], uint8_t tel);
+
+uint16_t getPulseWidth();
 
 int main(void)
 {
@@ -27,11 +38,44 @@ int main(void)
 	UCSR0B |= 1 << RXCIE0;
     init_master();
 	sei();
+	uint16_t PW; //Pulse width for US
+	writeString("Ultra Sonic Active"); //debug message
 
 	while (1)
 	{
+		US_DDR |= (1<<US_POS); //Set Ultra Sonic Port as out
+		_delay_us(10);
+		//Give the US pin a 15us High Pulse
+		US_PORT |= (1<<US_POS); //High
+		_delay_us(15);
+		US_PORT &= (~(1<<US_POS)); //Low
+		_delay_us(20);
+		
+		US_DDR &= (~(1<<US_POS)); //Make the US pin input
+		PW = getPulseWidth();
+		
+		//Handle Errors
+		if(PW == US_ERROR)
+		{
+			writeString("US ERROR!");
+		}
+		else if( PW == US_NO_OBSTACLE)
+		{
+			writeString("US no obstacle in range!");
+		}
+		//distance calculation
+		else
+		{
+			uint32_t distance;
+			distance = (PW/58); //convert to cm
+			uint8_t dist[4];
+			for( int i  = 0; i < 4; i++){
+				dist[i] = distance;
+				distance <<= 8;
+			}
+			verzenden_array(DEVICE_ADRES, dist, 4);
+		}
 	}
-
 }
 
 void verzenden_array(uint8_t address, uint8_t b[], uint8_t tel) {
@@ -73,4 +117,51 @@ void setMode(uint8_t set){
 ISR(USART0_RX_vect){
 	uint8_t data = UDR0;
 	sendControl(data);
+}
+
+uint16_t getPulseWidth()
+{
+	uint32_t i, result;
+	
+	//wait for the rising edge
+	for(i=0;i<600000;i++)
+	{
+		if(!(US_PIN & (1<<US_POS)))
+			continue;
+		else
+			break;
+	}
+	if(i == 600000)
+		return 0xffff; //indicates time out
+	
+	//High edge found
+	//Setup Timer1
+	TCCR1A = 0x00;
+	TCCR1B = (1<<CS11); //prescealer = Fcpu/8
+	TCNT1 = 0x00; //init counter
+	
+	//Now wait for the falling edge
+	for(i=0; i<600000;i++)
+	{
+		if(US_PIN & (1<<US_POS))
+		{
+			if(TCNT1 > 60000)
+				break;
+			else
+				continue;
+		}
+		else
+			break;
+	}
+	if(i == 60000)
+		return 0xffff; //indicates time out
+
+	result = TCNT1;// Falling edge found
+	TCCR1B = 0x00; //stop timer
+	
+	if(result > 60000)
+		return 0xfffe; // no obstacle in range
+	else
+		return (result >> 1);
+		
 }
