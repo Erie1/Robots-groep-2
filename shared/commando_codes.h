f54bd3b2877f706a4d02dd13b1a298984467eb0e
MSI-LAPTOP-OETZ\Oetze <oetze@live.nl>
2017-03-21 11:42:53 +0100

    ACK toegevoegd, Alles gecommented

--- shared/commando_codes.h
+++ shared/commando_codes.h
@@ -1,6 +1,8 @@
-#define COM_CONTROL 1 << 5S
-#define COM_AFSTANDRICHTING 2 << 5
-#define COM_PARCOURS 3 << 5
+#define COM_CONTROL 1 << 5S //De commando header die wordt gestuurd wanneer besturingsinformatie verstuurd wordt
+#define COM_AFSTANDRICHTING 2 << 5 //De commando header die wordt gestuurd wanneer een Astand en richting wordt vestuurd.
+#define COM_PARCOURS 3 << 5	//De commando header die wordt gestuur wanneer een parcourdata verzonden gaat worden.
+
+#define ACK 0xBB //De acknowledgement code die wordt gestuurd naar de computer om te zeggen dat alles goed is ontvangen.
 
 
 
